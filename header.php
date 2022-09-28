66f796349a0678a6a6a3492090e0874f78492b50
Joyex <loic_bienvenu@orange.fr>
2022-09-28 10:57:01 +0200

    Templates and general info (nd part)

---
+++ header.php
@@ -0,0 +1,95 @@
+<!-- ##### Header Area Start ##### -->
+<header class="header-area">
+
+<!-- Navbar Area -->
+<div class="delicious-main-menu">
+    <div class="classy-nav-container breakpoint-off">
+        <div class="container">
+            <!-- Menu -->
+            <nav class="classy-navbar justify-content-between" id="deliciousNav">
+
+                <!-- Logo -->
+                <a class="nav-brand" href="index.html"><img src="img/core-img/logo.png" alt=""></a>
+
+                <!-- Navbar Toggler -->
+                <div class="classy-navbar-toggler">
+                    <span class="navbarToggler"><span></span><span></span><span></span></span>
+                </div>
+
+                <!-- Menu -->
+                <div class="classy-menu">
+
+                    <!-- close btn -->
+                    <div class="classycloseIcon">
+                        <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                    </div>
+
+                    <!-- Nav Start -->
+                    <div class="classynav">
+                        <ul>
+                            <li class="active"><a href="index.html">Accueil</a></li>
+                            <li><a href="#">Pages</a>
+                                <ul class="dropdown">
+                                    <li><a href="index.html">Accueil</a></li>
+                                    <li><a href="cocktails-home.html">Cocktails</a></li>
+                                    <li><a href="receipe-home.html">Recettes</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Cocktails</a>
+                                <div class="cocktails">
+                                    <ul class="single-mega cn-col-4">
+                                        <li class="title">Gin</li>
+
+                                        <li><a href="gin-pegu-club.html">Pegu Club</a></li>
+                                        <li><a href="gin.html">Tous les cocktails</a></li>
+
+                                    </ul>
+                                    <ul class="single-mega cn-col-4">
+                                        <li class="title">Rhum</li>
+                                        
+                                    </ul>
+                                    <ul class="single-mega cn-col-4">
+                                        <li class="title">Whiskey</li>
+                                        <li><a href="whisky-template.html">Whisky Template</a></li>
+                                        
+                                    </ul>
+                                    <ul class="single-mega cn-col-4">
+                                        <li class="title">Tequila</li>
+                                            <li><a href="tequila-template.html">Tequila Template</a></li>
+
+                                        
+                                    </ul>
+                                    <ul class="single-mega cn-col-4">
+                                        <li class="title">Sirops</li>
+                                        
+                                    </ul>
+                                    <ul class="single-mega cn-col-4">
+                                        <li class="title">Mocktails</li>
+                                        
+                                    </ul>
+                                    <div class="single-mega cn-col-4">
+                                        <div class="receipe-slider owl-carousel">
+                                            <!-- <a href="#"><img src="img/bg-img/bg1.jpg" alt=""></a>
+                                            <a href="#"><img src="img/bg-img/bg6.jpg" alt=""></a> -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li><a href="#">Recettes</a>
+                                <ul class="dropdown">
+                                    <li><a href="index.html">recette 1</a></li>
+                                    <li><a href="index.html">recette 2</a></li>
+                                    <li><a href="index.html">recette 3</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- Nav End -->
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
+</header>
+<!-- ##### Header Area End ##### -->
